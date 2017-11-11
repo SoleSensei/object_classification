2d46abcd97224e08d6069a5aa87a1a4bc74ca4fc
SoleSensei <dimargfd@gmail.com>
2017-11-11 13:19:48 +0300

    added run scipts

--- run.sh
+++ run.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
+# script making and running classificator program and then calculate precision
 
-# without SSE
+# make project
+make
 
 cd ./build/bin
 
-
-# train classificator
+# --------------- without SSE ---------------
+# train classificator (no SSE)
+echo
+echo "Training (no SSE):"
+echo
 ./task2 -d ../../data/multiclass/train_labels.txt -m model.txt --train
 
-# test classificator
+# test classificator (no SSE)
+echo
+echo Predict:
+echo
 ./task2 -d ../../data/multiclass/test_labels.txt -m model.txt -l predict.txt --predict
 
 cd ../..
 
-#compare result
+# calculate precision
 echo 
 python compare.py ./data/multiclass/test_labels.txt ./build/bin/predict.txt 
 echo
-# using SSE
 
+# --------------- using SSE ---------------
 cd ./build/bin
 
-
 # train classificator
+echo
+echo "Training (SSE):"
+echo
 ./task2 -d ../../data/multiclass/train_labels.txt -m model.txt --train --sse
 
 # test classificator
+echo
+echo Predict:
+echo
 ./task2 -d ../../data/multiclass/test_labels.txt -m model.txt -l predict.txt --predict --sse
 
 cd ../..
---
+++ runTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# sudo bash runTest.sh # to begin tests
+
+# create build directory to make tests
+cd ./tests
+mkdir build
+
+# copy Make Lists for cmake
+cp ../bridge/CMakeLists.txt ./build/CMakeLists.txt
+echo
+
+# make tests
+echo Make Tests
+cd build 
+cmake CMakeLists.txt
+make
+echo 
+
+# run tests
+echo Running Tests 
+./runTests
+
+
