719c6989ef017ebf3c3f67fffdc5ad8cea8b2b4f
SoleSensei <dimargfd@gmail.com>
2017-11-13 10:49:36 +0300

    make files added

---
+++ build/deps/main.d
@@ -0,0 +1,6 @@
+build/obj/main.o build/deps/main.d: src/main.cpp include/classifier.h \
+ bridge/include/linear.h bridge/include/EasyBMP.h \
+ bridge/include/EasyBMP_DataStructures.h bridge/include/EasyBMP_BMP.h \
+ bridge/include/EasyBMP_VariousBMPutilities.h bridge/include/argvparser.h \
+ include/trainer.hpp include/Environment.h include/Timer.h \
+ include/matrix.h include/matrix.hpp include/classifier.h
---
+++ build/deps/trainer.d
@@ -0,0 +1,7 @@
+build/obj/trainer.o build/deps/trainer.d: src/trainer.cpp \
+ include/trainer.hpp include/Environment.h include/Timer.h \
+ include/matrix.h include/matrix.hpp include/classifier.h \
+ bridge/include/linear.h bridge/include/EasyBMP.h \
+ bridge/include/EasyBMP_DataStructures.h bridge/include/EasyBMP_BMP.h \
+ bridge/include/EasyBMP_VariousBMPutilities.h bridge/include/argvparser.h \
+ include/classifier.h
